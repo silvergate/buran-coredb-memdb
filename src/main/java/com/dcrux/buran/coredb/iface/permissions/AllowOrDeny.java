f5b8dd44d693c3a486a434634fdfd1a3239f7a1f
Stefan Secret <buran@dcrux.com>
2012-12-28 15:19:28 +0100

    replace with new header template

--- src/main/java/com/dcrux/buran/coredb/iface/permissions/AllowOrDeny.java
+++ src/main/java/com/dcrux/buran/coredb/iface/permissions/AllowOrDeny.java
@@ -1,11 +1,8 @@
 package com.dcrux.buran.coredb.iface.permissions;
 
 /**
- * Created with IntelliJ IDEA.
- * User: caelis
- * Date: 26.12.12
- * Time: 12:35
- * To change this template use File | Settings | File Templates.
+ *
+ * @author caelis
  */
 public enum AllowOrDeny {
   allow, deny
