b05b38cc7fb710288ee87eededdccb8624dad49a
silvergate <silvergate@gmail.com>
2013-01-19 18:42:22 +0100

    Versioning test geupdated mit einem Optimistic-Locking-Exception-Test.

--- src/test/java/com.dcrux.buran.coredb.iface/VersioningTest.java
+++ src/test/java/com.dcrux.buran.coredb.iface/VersioningTest.java
@@ -5,8 +5,10 @@ import com.dcrux.buran.coredb.iface.api.exceptions.*;
 import com.dcrux.buran.coredb.iface.base.TestsBase;
 import com.dcrux.buran.coredb.iface.common.NodeClassSimple;
 import com.dcrux.buran.coredb.iface.nodeClass.ClassId;
+import com.dcrux.buran.coredb.iface.propertyTypes.PrimGet;
 import com.dcrux.buran.coredb.iface.propertyTypes.PrimSet;
 import com.google.common.base.Optional;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -26,7 +28,8 @@ public class VersioningTest extends TestsBase {
     @Test
     public void createAndUpdateNode()
             throws PermissionDeniedException, IncubationNodeNotFound, OptimisticLockingException,
-            NodeNotUpdatable, HistoryHintNotFulfillable {
+            NodeNotUpdatable, HistoryHintNotFulfillable, NodeNotFoundException,
+            InformationUnavailableException {
         IApi api = getBuran();
 
         /* Create a node in incubation */
@@ -48,6 +51,7 @@ public class VersioningTest extends TestsBase {
 
         CommitResult commitResultOne = api.commit(getReceiver(), getSender(), iNidOne);
         NidVer nidVer0 = commitResultOne.getNid(iNidOne);
+        Assert.assertEquals("Should be first version", NidVer.FIRST_VERSION, nidVer0.getVersion());
 
         /* Create a new node in incubation and update 'nidVer0' node */
 
@@ -56,6 +60,88 @@ public class VersioningTest extends TestsBase {
                         nidVer0, Optional.<HistoryHint>absent());
         final IncNid iNidOneV2 = createInfoUpdate.getIncNid();
 
+        /* Populate node version 1 with some data */
+
+        int intValueV1 = 555543;
+        api.setData(getReceiver(), getSender(), iNidOneV2, NodeClassSimple.PROPERTY_INT,
+                PrimSet.integer(intValueV1));
+
+        /* Commit node version 1 */
+
         CommitResult commitResultTwo = api.commit(getReceiver(), getSender(), iNidOneV2);
+        NidVer nidVer1 = commitResultTwo.getNid(iNidOneV2);
+        Assert.assertEquals("Should be second version", NidVer.FIRST_VERSION + 1,
+                nidVer1.getVersion());
+        Assert.assertEquals("Node-ID from first and second version must be equal", nidVer0.getNid(),
+                nidVer1.getNid());
+
+        /* Check node states */
+        final NodeState v0State = api.getNodeState(getReceiver(), getSender(), nidVer0);
+        final NodeState v1State = api.getNodeState(getReceiver(), getSender(), nidVer1);
+        Assert.assertTrue("Version 0 Node must be in state 'historized'",
+                NodeState.isHistorized(v0State));
+        Assert.assertEquals("Version 1 Node must be in state 'active'", NodeState.available,
+                v1State);
+
+        /* Check properties of node v0 */
+        /* Checking is only possible if history information is still available */
+        if (v0State == NodeState.historizedAvailable) {
+            int rV0Int = (int) api
+                    .getData(getReceiver(), getSender(), nidVer0, NodeClassSimple.PROPERTY_INT,
+                            PrimGet.SINGLETON);
+            Assert.assertEquals(rV0Int, intValue);
+            String rV0Str = (String) api
+                    .getData(getReceiver(), getSender(), nidVer0, NodeClassSimple.PROPERTY_STRING,
+                            PrimGet.SINGLETON);
+            Assert.assertEquals(rV0Str, strValue);
+        }
+
+        /* Check properties of node v1 */
+        int rV1Int = (int) api
+                .getData(getReceiver(), getSender(), nidVer1, NodeClassSimple.PROPERTY_INT,
+                        PrimGet.SINGLETON);
+        Assert.assertEquals(rV1Int, intValueV1);
     }
+
+    @Test
+    public void optimisticLockingExceptionTest()
+            throws PermissionDeniedException, OptimisticLockingException, IncubationNodeNotFound,
+            NodeNotUpdatable, HistoryHintNotFulfillable {
+        IApi api = getBuran();
+
+        /* Create a node in incubation */
+        CreateInfo createInfo = api.createNew(getReceiver(), getSender(), this.classId,
+                Optional.<KeepAliveHint>absent());
+        IncNid iNidOne = createInfo.getIncNid();
+
+        /* Commit node */
+
+        CommitResult commitResultOne = api.commit(getReceiver(), getSender(), iNidOne);
+        NidVer nidVer0 = commitResultOne.getNid(iNidOne);
+        Assert.assertEquals("Should be first version", NidVer.FIRST_VERSION, nidVer0.getVersion());
+
+        /* Try to update node v0 twice. This should work until we try to commit. */
+        final CreateInfoUpdate createInfoUpdate11 =
+                api.createNewUpdate(getReceiver(), getSender(), Optional.<KeepAliveHint>absent(),
+                        nidVer0, Optional.<HistoryHint>absent());
+        final CreateInfoUpdate createInfoUpdate12 =
+                api.createNewUpdate(getReceiver(), getSender(), Optional.<KeepAliveHint>absent(),
+                        nidVer0, Optional.<HistoryHint>absent());
+        final IncNid iNidOneV11 = createInfoUpdate11.getIncNid();
+        final IncNid iNidOneV12 = createInfoUpdate12.getIncNid();
+
+        /* Commit first update node - this should work */
+        CommitResult commitResultTwo1 = api.commit(getReceiver(), getSender(), iNidOneV11);
+
+        boolean exceptionRaised = false;
+        try {
+        /* Commit second update node - this should raise an OptimisticLockingException */
+            CommitResult commitResultTwo2 = api.commit(getReceiver(), getSender(), iNidOneV12);
+        } catch (OptimisticLockingException ole) {
+            exceptionRaised = true;
+        }
+        Assert.assertTrue("The second commit must raise an OptimisticLockingException.",
+                exceptionRaised);
+    }
+
 }
